eafbd03476c8545c1b0a2767f6d3067ac19aee86
Ryan Yang <Ryanyang99@hotmail.com>
2018-07-30 14:06:44 -0700

    update

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ app.get('/getData', async (req, res) => {
 //shopify posts results to firebase cloudstore
 app.post('/postData', cors(), function(req, res){
     addVariant(req.body.pID,req.body.ETA,req.body.vID,req.body.inventory);// add to database
-  
+  res.send("Database updated successfully")
 });
 ///////////// Start the Server /////////////
 
