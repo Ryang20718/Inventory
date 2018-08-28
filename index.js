5e6c6f3ed351a502ff22fd4ff5972bd92039d894
Ryan Yang <Ryanyang99@hotmail.com>
2018-08-28 15:27:04 -0700

    update tasker update every hour

--- index.js
+++ index.js
@@ -919,7 +919,7 @@ getVariantRequireMsg().then(function(value) {
 
 
 //chron for deleting all customers who have been notified
-schedule.scheduleJob('* * * * 3', function(){//executes task once a week
+schedule.scheduleJob('* 3 * * *', function(){//executes task once an hour
 deleteNotifiedCustomer();
 });
 
